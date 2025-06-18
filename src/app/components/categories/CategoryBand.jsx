19942ec51cfa830408897054152feeefc9e1c19a
Rafael Dias Martins <rafaelmagister@gmail.com>
2025-06-18 17:35:46 -0300

    Enhancing style of category band

--- src/app/components/categories/CategoryBand.jsx
+++ src/app/components/categories/CategoryBand.jsx
@@ -2,26 +2,25 @@ import CategoryCard from './CategoryCard';
 import { getCategoryModel } from "@/app/models/Category";
 
 async function CategoryBand() {
-    await getCategoryModel();
-    const Category = await getCategoryModel();
-    const categories = await Category.find({ parent: null});
+  const Category = await getCategoryModel();
+  const categories = await Category.find({ parent: null });
 
-    return (
-        <div className="w-full py-1">
-            <div className="flex justify-center gap-4 px-2 max-w-screen-xl mx-auto flex-wrap">
-                {categories.map((category) => (
-                    <CategoryCard
-                        key={category.id}
-                        _id={category.id}
-                        title={category.title}
-                        slug={category.slug}
-                        imageUrl={category.imageUrl}
-                        emoji={category.emoji}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full py-1 overflow-x-auto m-3">
+      <div className="flex gap-4 px-2 max-w-screen-xl mx-auto flex-nowrap justify-center">
+        {categories.map((category) => (
+          <CategoryCard
+            key={category.id}
+            _id={category.id}
+            title={category.title}
+            slug={category.slug}
+            imageUrl={category.imageUrl}
+            emoji={category.emoji}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CategoryBand;
--- src/app/products/components/ProductCard.jsx
+++ src/app/products/components/ProductCard.jsx
@@ -21,7 +21,7 @@ function ProductCard({ name, imageUrl, price, description, urlLink }) {
         </div>
       </div>
       <h3 className="text-base font-semibold text-gray-900 dark:text-gray-500 text-center">{name}</h3>
-      <p className="text-sm text-gray-700 dark:text-gray-600 text-center">R$ {price}</p>
+      {/* <p className="text-sm text-gray-700 dark:text-gray-600 text-center">R$ {price}</p> */}
       {description && (
         <p className="text-xs text-gray-600 dark:text-gray-600 text-center">{description}</p>
       )}
